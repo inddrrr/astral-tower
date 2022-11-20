3db1a6ea2ffb2c0717047fa0deb59a23722486f9
Novian Deny <anggerdeni@gmail.com>
2022-11-20 16:17:03 +0700

    feat: add back to main menu button

--- Astral Tower/Scripts/GameOver.gd
+++ Astral Tower/Scripts/GameOver.gd
@@ -4,3 +4,9 @@ func _on_RestartButton_button_up():
 	get_tree().reload_current_scene()
 	get_tree().paused = false
 	self.queue_free()
+
+
+func _on_HomeButton_button_up():
+	if get_tree().change_scene("res://Scenes/MainMenu.tscn") != 0:
+		print("failed changing scene from GameOver HomeButton")
+	get_tree().paused = false
